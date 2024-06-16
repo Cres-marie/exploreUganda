804b28e18a33cdf66f86550bc38bfe8bfe7c5d68
mariecrescentia1 <mariecrescentia00@gmail.com>
2024-06-16 20:11:16 +0300

    --market

---
+++ lib/screens/marketScreens/Horeca.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Horeca extends StatefulWidget {
+  const Horeca({super.key});
+
+  @override
+  State<Horeca> createState() => _HorecaState();
+}
+
+class _HorecaState extends State<Horeca> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screens/marketScreens/Market.dart
+++ lib/screens/marketScreens/Market.dart
@@ -1,3 +1,9 @@
+import 'package:explore_uganda/components/constants/constants.dart';
+import 'package:explore_uganda/components/customComponents/CustomAppRow.dart';
+import 'package:explore_uganda/screens/marketScreens/AllMarket.dart';
+import 'package:explore_uganda/screens/marketScreens/Fleet.dart';
+import 'package:explore_uganda/screens/marketScreens/Horeca.dart';
+import 'package:explore_uganda/screens/marketScreens/TourGuide.dart';
 import 'package:flutter/material.dart';
 
 class Market extends StatefulWidget {
@@ -7,16 +13,90 @@ class Market extends StatefulWidget {
   State<Market> createState() => _MarketState();
 }
 
-class _MarketState extends State<Market> {
+class _MarketState extends State<Market>  with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+   @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+  }
+
   @override
+void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
 
-      body: Column(
-        children: [
-          Text('Market')
-        ],
+      body: SafeArea(
+        child: Padding(
+          padding: bpadding,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                
+                  Row(
+                    children: [
+                      
+                      Image.asset('assets/logo.png')
+                      
+                    ],
+                  ),
+
+                  CustomAppRow()
+                ]),
+
+                SizedBox(height: 15,),
+
+                Text('Market Place', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),),
+              
+              SizedBox(height: 15,),
+
+              TabBar(
+                  controller: _tabController,
+                  indicatorColor: appcolor,
+                  
+                  isScrollable: true,
+                  tabAlignment: TabAlignment.start,
+                  unselectedLabelColor: Colors.black87,
+                  labelColor: appcolor,
+                  tabs: [
+                   
+                    Tab(text: 'All'),
+                    Tab(text: 'Tour Guides & Operators'),
+                    Tab(text: 'HORECA'),
+                    Tab(text: 'Fleet Services'),
+                  
+              
+                  ],
+                ),
+              
+              Expanded(
+                child: TabBarView(
+                  controller: _tabController,
+                  children: const [
+                    AllMarket(),
+                    TourGuide(),
+                    Horeca(),
+                    Fleet(),
+                    
+                  
+                   
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
+
+
     );
   }
 }
